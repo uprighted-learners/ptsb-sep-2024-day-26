10ff0a81f8d3b8e8f3d11964fba34fc1bbd23071
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-11-14 16:41:56 -0600

    commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import axios from 'axios';
+import moment from 'moment';
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//     .catch(error => console.error(error)) 
+
+axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error))
+
+const now = moment();
+console.log("Current date and time", now.format('LLLL'));
